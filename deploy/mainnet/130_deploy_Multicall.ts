9ed57a04f332c5f14dbdb8ffb0b983b0aa984ed9
Michael Daly <dalymike102@gmail.com>
2022-03-17 14:20:19 -0400

    ran lint

--- deploy/mainnet/130_deploy_Multicall.ts
+++ deploy/mainnet/130_deploy_Multicall.ts
@@ -1,9 +1,7 @@
 import { DeployFunction } from "hardhat-deploy/types"
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { impersonateAccount } from "../../test/testUtils"
-import { ethers, hardhatArguments } from "hardhat"
-
-
+import { ethers } from "hardhat"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre
@@ -22,21 +20,23 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     skipIfAlreadyDeployed: true,
   })
 
-  const contractOwnerAddress = '0x05f32b3cc3888453ff71b01135b34ff8e41263f2'
+  const contractOwnerAddress = "0x05f32b3cc3888453ff71b01135b34ff8e41263f2"
   const impersonatedOwner = await impersonateAccount(contractOwnerAddress)
   await hre.network.provider.send("hardhat_setBalance", [
     await impersonatedOwner.getAddress(),
     `0x${(1e18).toString(16)}`,
   ])
-  const Multicall3 = await ethers.getContractFactory("Multicall3", impersonatedOwner);
-  const multicall3 = await Multicall3.deploy();
-  await multicall3.deployed();
+  const Multicall3 = await ethers.getContractFactory(
+    "Multicall3",
+    impersonatedOwner,
+  )
+  const multicall3 = await Multicall3.deploy()
+  await multicall3.deployed()
   await save("Multicall3", {
     abi: multicall3.abi,
     address: multicall3.address,
   })
-  console.log("multicall3 deployed to:", multicall3.address);
-
+  console.log("multicall3 deployed to:", multicall3.address)
 }
 export default func
 func.tags = ["Multicall"]
